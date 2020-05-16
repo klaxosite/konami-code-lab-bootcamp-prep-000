e5e832e9becf61a8d02815326256895af1df8181
Jastien Sandhu <superjastin@gmail.com>
2020-05-16 00:55:37 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -21,13 +21,12 @@ function init() {
   //variable for keypresses  
   const key = event.key;
   let index = 0;
-  
   if (key === codes[index]){
     index++;
     if(index === codes.length){
       alert("KONAMI!!! ");
       index = 0;
-    }1`Int16Array
+    }
   } else {
     index = 0;
   }
